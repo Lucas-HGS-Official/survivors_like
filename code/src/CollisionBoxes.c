abefeef38fa91dec60ef362c57fbd00b49fe426f
Lucas_hgs <lucas.henrique.gois.santos@gmail.com>
2026-07-18 08:44:12 -0300

    Fixing create_collision_recs_list

--- code/src/CollisionBoxes.c
+++ code/src/CollisionBoxes.c
@@ -5,7 +5,7 @@ CollisionRecs *create_collision_recs_list(Rectangle *recs, CollisionType box_typ
     CollisionRecs *collision_recs_list = (CollisionRecs*) MemAlloc(sizeof(CollisionRecs));
     collision_recs_list->num = recs_num;
 
-    collision_recs_list->box_list = (CollisionBox*) MemAlloc(sizeof(Rectangle) * recs_num);
+    collision_recs_list->box_list = (CollisionBox*) MemAlloc(sizeof(CollisionBox) * recs_num);
 
     for (int i=0; i<collision_recs_list->num; i++) {
         collision_recs_list->box_list[i].rec = recs[i];
