8e243dde39118561ff6e2b0f116ee993b76f1f2a
Lucas_hgs <lucas.henrique.gois.santos@gmail.com>
2026-07-22 11:50:14 -0300

    Refactoring the collision system

--- code/include/CollisionBoxes.h
+++ code/include/CollisionBoxes.h
@@ -4,28 +4,29 @@
 #include <stdbool.h>
 
 
-// typedef enum CollisionType {
-//     NONE_COLLISION_TYPE,
+typedef enum CollisionType {
+    NONE_COLLISION_TYPE,
 
-//     ENV_COLLISION_TYPE,
-//     ENEMY_COLLISION_TYPE,
-//     PLAYER_COLLISION_TYPE,
-//     BULLET_COLLISION_TYPE,
+    ENV_COLLISION_TYPE,
+    ENEMY_COLLISION_TYPE,
+    PLAYER_COLLISION_TYPE,
+    BULLET_COLLISION_TYPE,
 
-//     NUM_COLLISION_TYPES,
-// } CollisionType;
+    NUM_COLLISION_TYPES,
+} CollisionType;
 
-// typedef struct CollisionBox {
-//     Rectangle rec;
-//     CollisionType type;
-// } CollisionBox;
+typedef struct CollisionBox {
+    Rectangle rec;
+    CollisionType type;
+} CollisionBox;
 
-// typedef struct CollisionBoxList {
-//     CollisionBox *list;
-//     int size;
-// } CollisionBoxList;
+typedef struct CollisionBoxList {
+    int size;
+    CollisionType type;
+    Rectangle *list;
+} CollisionBoxList;
 
 
-// CollisionBoxList *create_collision_box_list(Rectangle *recs, CollisionType box_type, int recs_num);
-// CollisionBox check_collision_box_list(CollisionBox box, CollisionBoxList **box_list);
-// void destroy_collision_box_list(CollisionBoxList *collision_recs_list);
+void create_collision_box_list(CollisionBoxList *collision_boxes, Rectangle *recs, CollisionType box_type, int num_recs);
+CollisionBox check_collision_box_list(CollisionBox box, CollisionBoxList *collision_boxes);
+void destroy_collision_box_list(CollisionBoxList *collision_boxes);
--- code/src/CollisionBoxes.c
+++ code/src/CollisionBoxes.c
@@ -1,56 +1,65 @@
 #include "CollisionBoxes.h"
 
-
-// bool _is_collision_box_identical(CollisionBox box1, CollisionBox box2);
-
-// CollisionBoxList *create_collision_box_list(Rectangle *recs, CollisionType box_type, int recs_num) {
-//     CollisionBoxList *collision_recs_list = (CollisionBoxList*) MemAlloc(sizeof(CollisionBoxList));
-//     collision_recs_list->size = recs_num;
-
-//     collision_recs_list->list = (CollisionBox*) MemAlloc(sizeof(CollisionBox) * recs_num);
-
-//     for (int i=0; i<collision_recs_list->size; i++) {
-//         collision_recs_list->list[i].rec = recs[i];
-//         collision_recs_list->list[i].type = box_type;
-//     }
-
-//     return collision_recs_list;
-// }
-
-// CollisionBox check_collision_box_list(CollisionBox box, CollisionBoxList **box_list) {
-//     CollisionBox collided_box = {0};
-
-//     for (int i=0; i<NUM_COLLISION_TYPES; i++) {
-//         for (int j=0; j<box_list[i]->size; j++) {
-//             if (_is_collision_box_identical(box, box_list[i]->list[j])) {
-//                 continue;
-//             } else if (CheckCollisionRecs(box.rec, box_list[i]->list[j].rec)) {
-//                 collided_box = box_list[i]->list[j];
-//                 return collided_box;
-//             }
-//         }
-//     }
-
-//     return collided_box;
-// }
-
-// void destroy_collision_box_list(CollisionBoxList *collision_recs_list) {
-//     MemFree(collision_recs_list->list);
-//     MemFree(collision_recs_list);
-
-//     return;
-// }
+#include <raylib.h>
+#include <stdlib.h>
 
 
-// bool _is_collision_box_identical(CollisionBox box1, CollisionBox box2) {
-//     if (box1.rec.x != box2.rec.x && box1.rec.y != box2.rec.y) {
-//         return false;
-//     }
-//     if (box1.rec.width != box2.rec.width && box1.rec.height != box2.rec.height) {
-//         return false;
-//     }
-//     if (box1.type != box2.type) {
-//         return false;
-//     }
-//     return true;
-// }
+bool _is_collision_box_identical(CollisionBox box1, CollisionBox box2);
+
+void create_collision_box_list(CollisionBoxList *collision_boxes, Rectangle *recs, CollisionType box_type, int num_recs) {
+    collision_boxes[box_type].size = num_recs;
+    collision_boxes[box_type].type = box_type;
+
+    collision_boxes[box_type].list = (Rectangle*)MemAlloc(sizeof(Rectangle)*num_recs);
+
+    for (int i=0; i<collision_boxes[box_type].size; i++) {
+        collision_boxes[box_type].list[i] = recs[i];
+    }
+}
+
+CollisionBox check_collision_box_list(CollisionBox box, CollisionBoxList *collision_boxes) {
+    CollisionBox collided_box = {0};
+
+    for (int i=0; i<NUM_COLLISION_TYPES; i++) {
+        for (int j=0; j<collision_boxes[i].size; j++) {
+            collided_box.rec = collision_boxes[i].list[j];
+            collided_box.type = collision_boxes[i].type;
+
+            if (collided_box.rec.height == 0.f || collided_box.rec.width == 0.f) {
+                continue;
+            } else if (_is_collision_box_identical(box, collided_box)) {
+                continue;
+            } else if (CheckCollisionRecs(box.rec, collided_box.rec)) {
+                return collided_box;
+            }
+        }
+    }
+
+    collided_box = (CollisionBox) {0};
+
+    return collided_box;
+}
+
+void destroy_collision_box_list(CollisionBoxList *collision_boxes) {
+    for (int i=0; i<NUM_COLLISION_TYPES; i++) {
+        if (collision_boxes[i].list != NULL) {
+            MemFree(collision_boxes[i].list);
+        }
+    }
+
+    return;
+}
+
+
+bool _is_collision_box_identical(CollisionBox box1, CollisionBox box2) {
+    if (box1.rec.x != box2.rec.x && box1.rec.y != box2.rec.y) {
+        return false;
+    }
+    if (box1.rec.width != box2.rec.width && box1.rec.height != box2.rec.height) {
+        return false;
+    }
+    if (box1.type != box2.type) {
+        return false;
+    }
+    return true;
+}
